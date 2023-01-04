1b6f6fbdf55098dabc8373c6180959eca19cc93e
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-01-04 23:59:56 +0530

    Add AAA method for tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,11 +2,11 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
-  render(<App />); // simulate
+  render(<App />); // arrange
 
-  // do something - mimick events
+  // act
 
-  const linkElement = screen.getByText(/learn react/i); // inspect
-
-  expect(linkElement).toBeInTheDocument(); // assert
+  // assert
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
