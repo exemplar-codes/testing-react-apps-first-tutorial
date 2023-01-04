3e98669a7337771416494341af21c33c59ca0376
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-01-04 23:54:23 +0530

    Highlight general steps for testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders learn react link", () => {
+  render(<App />); // simulate
+
+  // do something - mimick events
+
+  const linkElement = screen.getByText(/learn react/i); // inspect
+
+  expect(linkElement).toBeInTheDocument(); // assert
 });
