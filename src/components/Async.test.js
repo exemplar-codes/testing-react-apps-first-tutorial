03c9b723f867a32dcdf917db6000707e1cdee466
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-01-08 04:10:03 +0530

    Mock fetch function using jest.fn()

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -17,4 +17,25 @@ describe("Async component", () => {
 
     expect(listItems).not.toHaveLength(0);
   });
+
+  test("renders posts if request succeeds - mocked", async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [
+        {
+          userId: 1,
+          id: 1,
+          title:
+            "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+          body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+        },
+      ],
+    }); // Keep mock consistent -  fetch returns an object than has an async function called `json`.
+
+    render(<Async />);
+
+    const listItems = await screen.findAllByRole("listitem");
+
+    expect(listItems).not.toHaveLength(0);
+  });
 });
