b05bd19911e230a97a4570961dd664bee2636c2b
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-01-08 03:23:01 +0530

    How time waited async test works

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -5,7 +5,15 @@ describe("Async component", () => {
   test("renders posts if request succeeds", async () => {
     render(<Async />);
 
-    const listItems = await screen.findAllByRole("listitem");
+    const listItems = await screen.findAllByRole(
+      "listitem",
+      {
+        /** exact etc */
+      },
+      {
+        /* wait options - default wait time is 1 second, can be changed */
+      }
+    ); // re-inspects virtual UI for specified wait conditions
 
     expect(listItems).not.toHaveLength(0);
   });
